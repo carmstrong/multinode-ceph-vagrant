5f90f94cde9365c406b570a6c0185eb2147a337f
sailor <segler_alex@web.de>
2019-08-10 12:52:07 +0200

    init restructuring, added rgw and dashboard

--- init.sh
+++ init.sh
@@ -4,35 +4,58 @@ export CEPH_RELEASE="nautilus"
 
 set -e
 
+# add ceph repository
 wget -q -O- 'https://download.ceph.com/keys/release.asc' | sudo apt-key add -
 echo deb https://download.ceph.com/debian-${CEPH_RELEASE}/ $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/ceph.list
 
-DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -yq ntp ceph-deploy
+# install self
+DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -yq ntp ceph-deploy jq
 
+# Keys
 mkdir -p /root/.ssh
 cp .ssh/id_rsa .ssh/id_rsa.pub /root/.ssh
 cp .ssh/id_rsa.pub /root/.ssh/authorized_keys
+ssh-keyscan -H -t rsa ceph-server-1 ceph-server-2 ceph-server-3 ceph-client > /root/.ssh/known_hosts
+
+# setup config
 mkdir -p test-cluster
 cd test-cluster
-ssh-keyscan -H -t rsa ceph-server-1 ceph-server-2 ceph-server-3 ceph-client > /root/.ssh/known_hosts
 ceph-deploy install --release=${CEPH_RELEASE} ceph-admin ceph-server-1 ceph-server-2 ceph-server-3 ceph-client
 ceph-deploy new ceph-server-1 ceph-server-2 ceph-server-3
 echo "mon_clock_drift_allowed = 1" >> ceph.conf
+echo "[mon]" >> ceph.conf
+echo "mon-allow-pool-delete = true" >> ceph.conf
+
+# other dependencies
+ssh ceph-server-1 DEBIAN_FRONTEND=noninteractive apt install -yq ceph-mgr-dashboard python-routes
+
+# ceph-deploy
 ceph-deploy mon create-initial
-
 ceph-deploy admin ceph-admin ceph-server-1 ceph-server-2 ceph-server-3 ceph-client
+ceph-deploy mgr create ceph-server-1
+ceph-deploy rgw create ceph-server-1
+ceph-deploy mds create ceph-server-1
 
+# ceph modules
+ceph mgr module enable dashboard
+ceph dashboard create-self-signed-cert
+
+# keyrings
 chmod +r /etc/ceph/ceph.client.admin.keyring
 ssh ceph-server-1 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
 ssh ceph-server-2 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
 ssh ceph-server-3 sudo chmod +r /etc/ceph/ceph.client.admin.keyring
 
-ceph-deploy mgr create ceph-server-1
-
-#ssh ceph-server-2 "sudo mkdir /var/local/osd0 && sudo chown ceph:ceph /var/local/osd0"
-#ssh ceph-server-3 "sudo mkdir /var/local/osd1 && sudo chown ceph:ceph /var/local/osd1"
-#ceph-deploy osd prepare ceph-server-2:/var/local/osd0 ceph-server-3:/var/local/osd1
-#ceph-deploy osd activate ceph-server-2:/var/local/osd0 ceph-server-3:/var/local/osd1
+# setup osd
 ceph-deploy osd create --bluestore --data /dev/sdc ceph-server-1
 ceph-deploy osd create --bluestore --data /dev/sdc ceph-server-2
 ceph-deploy osd create --bluestore --data /dev/sdc ceph-server-3
+
+# rgw binding to dashboard
+ADMIN_USER=$(radosgw-admin user create --uid=admin --display-name=admin --system)
+ACCESS_KEY=$(echo $ADMIN_USER | jq ".keys[0].access_key")
+SECRET_KEY=$(echo $ADMIN_USER | jq ".keys[0].secret_key")
+ceph dashboard set-rgw-api-access-key $ACCESS_KEY
+ceph dashboard set-rgw-api-secret-key $SECRET_KEY
+
+# ceph dashboard ac-user-create administrator password administrator
